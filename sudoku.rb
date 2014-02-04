00ec2e7c38c0312e9f8e1c2b212a6f2d59849c21
Abraham Thomas <dysruptmedia@gmail.com>
2014-02-04 14:51:50 +0000

    Filled out grid

--- sudoku.rb
+++ sudoku.rb
@@ -1,5 +1,20 @@
 require 'sinatra' # load sinatra
+require_relative './lib/sudoku'
+require_relative './lib/cell'
 
-get '/' do # default route for our website
+
+def random_sudoku
+    # we're using 9 numbers, 1 to 9, and 72 zeros as an input
+    # it's obvious there may be no clashes as all numbers are unique
+    seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
+    sudoku = Sudoku.new(seed.join)
+    # then we solve this (really hard!) sudoku
+    sudoku.solve!
+    # and give the output to the view as an array of chars
+    sudoku.to_s.chars
+end
+
+get '/' do
+  @current_solution = random_sudoku
   erb :index
 end
